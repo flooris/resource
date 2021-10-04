386bb5b1e139f74c3ab6bb47f8830b338a6cc778
Rik Zwarthoff <zwarthoffr@gmail.com>
2021-10-04 11:48:43 +0200

    Fixed missing config file

---
+++ config/resource.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'field_components' => [
+        \Flooris\Resource\Fields\Actions::class       => 'ActionsField',
+        \Flooris\Resource\Fields\Badge::class         => 'BadgeField',
+        \Flooris\Resource\Fields\Boolean::class       => 'BooleanField',
+        \Flooris\Resource\Fields\Currency::class      => 'CurrencyField',
+        \Flooris\Resource\Fields\Date::class          => 'DateField',
+        \Flooris\Resource\Fields\DateTime::class      => 'DateTimeField',
+        \Flooris\Resource\Fields\Number::class        => 'NumberField',
+        \Flooris\Resource\Fields\Password::class      => 'PasswordField',
+        \Flooris\Resource\Fields\RadioSelect::class   => 'RadioSelectField',
+        \Flooris\Resource\Fields\Rating::class        => 'RatingField',
+        \Flooris\Resource\Fields\RelationCount::class => 'RelationCountField',
+        \Flooris\Resource\Fields\Select::class        => 'SelectField',
+        \Flooris\Resource\Fields\Text::class          => 'TextField',
+        \Flooris\Resource\Fields\Textarea::class      => 'TextAreaField',
+    ],
+];
